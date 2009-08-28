ab20f08d5297c1300828c9d684748289087e5725
Sava Chankov <sava@tutuf.com>
2009-08-28 16:02:42 +0300

    Don't attempt to connect to the database if there is no connection
    Thanks to LeFnord for reporting (http://github.com/LeFnord)

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 class ClassTableInheritanceAdapterNotCompatibleError < StandardError; end
 
 require 'instantiate_with_clti_support'
-if ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+if ActiveRecord::Base.connected? && ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
   require 'postgresql_adapter'
-else
+elsif ActiveRecord::Base.connected?
   raise ClassTableInheritanceAdapterNotCompatibleError.new("Only PostgreSQL is currently supported by the class table inheritance plugin.")
 end
 
