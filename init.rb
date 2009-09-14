abba385f165f1b8cc4a3836eae3d15ad91ccb0c3
Sava Chankov <sava@tutuf.com>
2009-09-14 13:19:58 +0300

    Fixed init.rb so tests run again

--- init.rb
+++ init.rb
@@ -1,10 +1,9 @@
 class ClassTableInheritanceAdapterNotCompatibleError < StandardError; end
 
 require 'instantiate_with_clti_support'
-if ActiveRecord::Base.connected?
-  if ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-    require 'postgresql_adapter'
-  else
-    raise ClassTableInheritanceAdapterNotCompatibleError.new("Only PostgreSQL is currently supported by the class table inheritance plugin.")
-  end
+
+unless defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  raise ClassTableInheritanceAdapterNotCompatibleError.new("Only PostgreSQL is currently supported by the class table inheritance plugin.")
 end
+
+require 'postgresql_adapter'
