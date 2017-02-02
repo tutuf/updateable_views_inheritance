e26d9b9d75b6acb40d7ff3fe45abf8eb9e69214f
Sava Chankov <sava@tutuf.com>
2017-02-02 16:08:16 +0100

    Upgrade install generator for Rails 4

--- lib/generators/updateable_views_inheritance/install_generator.rb
+++ lib/generators/updateable_views_inheritance/install_generator.rb
@@ -6,12 +6,16 @@ module UpdateableViewsInheritance
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-      extend ActiveRecord::Generators::Migration
-      
-      desc "Creates a migration that creates a special table used by the updateable_views_inheritance gem."
+
+      desc "Creates a migration for a special table used by the updateable_views_inheritance gem"
       source_root File.expand_path('../templates', __FILE__)
-      
-      def create_migration
+
+      # Implement the required interface for Rails::Generators::Migration
+      def self.next_migration_number(dirname)
+        ActiveRecord::Generators::Base.next_migration_number(dirname)
+      end
+
+      def install
         migration_file = "create_updateable_views_inheritance.rb"
         migration_template migration_file, "db/migrate/#{migration_file}"
       end
