fcbb7ad139e357befb9d0a23b8e4555f8725cb91
Sava Lachezarov <sava@tutuf.com>
2025-01-30 16:28:33 +0200

    No abstract classes in inheritance chain

--- test/dummy/app/models/locomotive.rb
+++ test/dummy/app/models/locomotive.rb
@@ -1,3 +1,2 @@
 class Locomotive < ActiveRecord::Base
-  abstract_class = true
 end
--- test/dummy/app/models/vehicle.rb
+++ test/dummy/app/models/vehicle.rb
@@ -1,4 +1,3 @@
 class Vehicle < ActiveRecord::Base
-  abstract_class = true
   self.inheritance_column = :vehicle_type
 end
